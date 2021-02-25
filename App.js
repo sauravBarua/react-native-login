b655422a09de961c0c0202f5054f2ae939734590
sauravBarua <myallmailofficial@gmail.com>
2021-02-25 16:55:14 +0100

    working on it

--- App.js
+++ App.js
@@ -1,55 +1,27 @@
-import {StyleSheet, Text, View, TextInput,Pressable } from 'react-native';
-import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+import Home from "./components/Home"
+import Login from "./components/Login"
 
-const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
- 
-  const onSubmit = () => {
-    if (password.length <= 6) {
-      alert('Password minimum 6 characters');
-    } else {
-      alert('Form submited');
-    }
-  }
-  return (
-    <View style={styles.container}>
-      <TextInput
-        onChangeText={text => setEmail(text)}
-        value={email}
-        placeholder={'Email'}
-        style={styles.inputStyle}
-      />
-      <TextInput
-        onChangeText={value => setPassword(value)}
-        placeholder={'Password'}
-        style={styles.inputStyle}
-        secureTextEntry={true}
-      />
-      <Pressable
-        style={styles.inputStyle}
-        onPress={onSubmit}>
-        <Text style={{ textAlign: "center" }}> login </Text>
-      </Pressable>
-    </View>
-  );
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <NativeRouter>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/Home" component={Home} />
+            </NativeRouter>;
+            <StatusBar style="auto" />
+        </View>
+    );
 }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputStyle: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
-export default App;

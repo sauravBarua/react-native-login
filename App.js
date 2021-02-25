03ee0833a529abca93ded38a8100b472c8028068
sauravBarua <myallmailofficial@gmail.com>
2021-02-25 17:05:08 +0100

    update

--- App.js
+++ App.js
@@ -2,26 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeRouter, Route, Link } from "react-router-native";
-import Home from "./components/Home"
 import Login from "./components/Login"
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <NativeRouter>
+        <NativeRouter>
             <Route exact path="/" component={Login} />
-            <Route exact path="/Home" component={Home} />
-            </NativeRouter>;
-            <StatusBar style="auto" />
-        </View>
+        </NativeRouter>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View, TextInput,Pressable } from 'react-native';
 import React, { useState } from 'react';
+import { Link } from "react-router-native";
 const App = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
